5031cf8fa67f5cc76e205dd83ba4006b12086785
sathishkumar67 <sathishvillers2000@gmail.com>
2024-08-06 09:30:57 +0530

    updated for 22th_30mtokens_model

--- train.py
+++ train.py
@@ -17,15 +17,15 @@ from schedulefree.adamw_schedulefree import AdamWScheduleFree
 
 login_token = "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"
 
-file_data1 = "edufineweb_train_000061.npy"
-file_data2 = "edufineweb_train_000062.npy"
-file_data3 = "edufineweb_train_000063.npy"
+file_data1 = "edufineweb_train_000064.npy"
+file_data2 = "edufineweb_train_000065.npy"
+file_data3 = "edufineweb_train_000066.npy"
 
-ckpt_file = "20th_30mtokens_model.ckpt"
+ckpt_file = "21th_30mtokens_model.ckpt"
 
-log_name = "21th_30mtokens_model"
+log_name = "22th_30mtokens_model"
 
-model_upload_name = "21th_30mtokens_model.ckpt"
+model_upload_name = "22th_30mtokens_model.ckpt"
 
 # logging in to the hugging face
 login(login_token)
