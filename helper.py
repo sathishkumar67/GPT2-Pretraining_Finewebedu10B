1ffee563c7a0a4c04d2c8bf37731222e0abad9d8
sathishkumar67 <sathishvillers2000@gmail.com>
2024-09-10 19:23:24 +0530

    added helper file

---
+++ helper.py
@@ -0,0 +1,12 @@
+from huggingface_hub import login, hf_hub_download, HfApi
+
+# upload the model
+api = HfApi()
+
+# fileuploader
+api.upload_file(
+    path_or_fileobj=f"logs/{log_name}/version_0/checkpoints/epoch=0-step=12207.ckpt",
+    path_in_repo=f"2nd_epoch/{log_name}.ckpt",
+    repo_id="pt-sk/GPT2_pretrained_finewebedu10B",
+    repo_type="model",
+)
